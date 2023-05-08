a1a09e36c74d0fb1f63294df387e9b5ec675c618
Bryan Aldrin Quinalayo <bryan@pinion.ph>
2023-05-08 21:21:43 +0800

    feat(updates): dark mode and contentful integration

---
+++ src/lib/contentful.ts
@@ -0,0 +1,20 @@
+import contentful, { EntrySkeletonType } from "contentful";
+import type { Document } from "@contentful/rich-text-types";
+
+export interface HomeCard extends EntrySkeletonType {
+  title: string,
+  url: string,
+  iconType: string
+}
+
+export interface IntroParagraph extends EntrySkeletonType {
+  body: Document
+}
+
+export const contentfulClient = contentful.createClient({
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+});
