2d9afff6f86b812a6a519e054305a140a9b00e48
Bryan Aldrin Quinalayo <bryan@pinion.ph>
2023-05-08 21:31:27 +0800

    feat(analytics): add vercek analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -7,4 +8,5 @@ export default defineConfig({
       external: ['svgo']
     }
   },
+  integrations: [react()]
 });
